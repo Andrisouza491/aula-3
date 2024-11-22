e29e0402e3e6040b0556aef598de7af6a15be9c0
Dri <andrilene26@gmail.com>
2024-11-22 11:41:42 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ListaDeContatos from './ListaDeContatos';
+import CalculadoraIMC from './CalculadoraIMC';
+
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <h1>Aplicações React</h1>
+      <h2>Lista de Contatos</h2>
+      <ListaDeContatos />
+      <h2>Calculadora de IMC</h2>
+      <CalculadoraIMC peso={70} altura={1.75} />
+    </div>
+  );
+}
+
+export default App;
